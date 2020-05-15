9ecbfc6226ba2d126d2ef101ef61aa96a03ef6f7
svr8 <shikhar.vaish90@gmail.com>
2020-05-15 17:31:10 +0530

    bash test

--- test.bash
+++ test.bash
@@ -1,2 +1,48 @@
-TEST_VAR=message
-echo prefix-$TEST_VAR;
+ACR_NAME=xyz2020
+GIT_USER=svr8
+TASK_NAME=flasktask
+
+git pull origin master
+
+RES_GROUP=$ACR_NAME # Resource Group name
+az group create --resource-group $RES_GROUP --location eastus
+az acr create --resource-group $RES_GROUP --name $ACR_NAME --sku Standard --location eastus
+az acr build --registry $ACR_NAME --image $TASK_NAME:v1 .
+
+# Registry authentication
+AKV_NAME=$ACR_NAME-vault
+az keyvault create --resource-group $RES_GROUP --name $AKV_NAME
+
+# Create service principal, store its password in AKV (the registry *password*)
+az keyvault secret set \
+  --vault-name $AKV_NAME \
+  --name $ACR_NAME-pull-pwd \
+  --value $(az ad sp create-for-rbac \
+                --name $ACR_NAME-pull \
+                --scopes $(az acr show --name $ACR_NAME --query id --output tsv) \
+                --role acrpull \
+                --query password \
+                --output tsv)
+
+# Store service principal ID in AKV (the registry *username*)
+az keyvault secret set \
+    --vault-name $AKV_NAME \
+    --name $ACR_NAME-pull-usr \
+    --value $(az ad sp show --id http://$ACR_NAME-pull --query appId --output tsv)
+
+# Store service principal ID in AKV (the registry *username*)
+az keyvault secret set \
+    --vault-name $AKV_NAME \
+    --name $ACR_NAME-pull-usr \
+    --value $(az ad sp show --id http://$ACR_NAME-pull --query appId --output tsv)
+# Deploy a container with Azure CLI
+az container create \
+    --resource-group $RES_GROUP \
+    --name acr-tasks \
+    --image $ACR_NAME.azurecr.io/$TASK_NAME:v1 \
+    --registry-login-server $ACR_NAME.azurecr.io \
+    --registry-username $(az keyvault secret show --vault-name $AKV_NAME --name $ACR_NAME-pull-usr --query value -o tsv) \
+    --registry-password $(az keyvault secret show --vault-name $AKV_NAME --name $ACR_NAME-pull-pwd --query value -o tsv) \
+    --dns-name-label acr-tasks-$ACR_NAME \
+    --query "{FQDN:ipAddress.fqdn}" \
+    --output table
