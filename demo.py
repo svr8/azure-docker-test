90a86180080dbc5b235b9e2a46564e30cff47473
svr8 <shikhar.vaish90@gmail.com>
2020-05-17 00:09:18 +0530

    test trigger

--- demo.py
+++ demo.py
@@ -3,8 +3,7 @@ app = Flask(__name__)
   
 @app.route('/') 
 def hello(): 
-    return "sample7"
-  
+    return "sample9"
   
 if __name__ == "__main__": 
     app.run(host ='0.0.0.0', port = 80, debug = True)  
